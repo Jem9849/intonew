2d5c7b4c1b5f9d31360ee4233f349dbc21814258
Jordan Hudgens <jordan@devcamp.com>
2017-06-15 15:44:56 +0000

    newie 2

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,23 @@ class PortfoliosController < ApplicationController
   def index
      @portfolios = Portfolio.all
   end
+  
+  def new 
+    @portfolios = Portfolio.new
+  end
+  
+  def create
+    @portfolios = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolios.save
+        format.html { redirect_to portfolios_path, notice: 'Blew blah live LIVE' }
+        format.json { render :show, status: :created, location: @portfolios }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolios.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
 end
