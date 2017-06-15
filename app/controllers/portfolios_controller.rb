dfa40775ba604801bddae3404ac454f16588bcd7
Jordan Hudgens <jordan@devcamp.com>
2017-06-15 16:09:32 +0000

    edit functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -20,5 +20,20 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  def edit
+    @portfolios = Portfolio.find(params[:id])
+  end
+  
+def update
+ @portfolios = Portfolio.find(params[:id])
+ 
+ respond_to do |format|
+  if @portfolios.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+    format.html { redirect_to portfolios_path, notice: 'IT WAS ALL UPDATED. NOW GO AWAY' }
+ else
+   format.html { render :edit }
+  end
+ end
+end
   
 end
