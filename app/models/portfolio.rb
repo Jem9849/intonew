9972e4a1c9aa08c837d178733b4dcff7ab794b3b
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 16:33:27 +0000

    concerns

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+  include Placeholder
   
   def self.angular
     where(subtitle: 'Angular')
@@ -11,6 +12,6 @@ class Portfolio < ApplicationRecord
   
   def set_defaults
     self.main_image ||= "http://www.pawschicago.org/fileadmin/media/images/adoption_section/PAWS_New_Site__51_of_201__01.jpg"
-    self.thumb_image ||= "https://i1.wp.com/amyshojai.com/wp-content/uploads/2015/03/CatFall_33186977_original.jpg?fit=1200%2C1200&resize=350%2C200"
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
