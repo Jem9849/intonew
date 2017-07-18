a5dc9b327c1fdc33d64a8b173b7ad9beec237113
Jordan Hudgens <jordan@devcamp.com>
2017-07-18 15:56:07 +0000

    implemented persistent position update for drag and drop

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,14 @@ site_admin: :all
      @portfolios = Portfolio.by_position
   end
   
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+  
+  render nothing: true
+  end
+  
    def angular
      @angular_portfolio_items = Portfolio.angular
    end
